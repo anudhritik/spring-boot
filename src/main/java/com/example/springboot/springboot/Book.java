8d18eca4fd42517a5facf618f15d57bfd21080b3
anudhritik <anukreddy28@gmail.com>
2019-04-16 23:16:05 -0400

    Adding model and controller

---
+++ src/main/java/com/example/springboot/springboot/Book.java
@@ -0,0 +1,20 @@
+package com.example.springboot.springboot;
+
+public class Book {
+	long id;
+	String name;
+	String author;
+
+	public Book(long id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", author=" + author + "]";
+	}
+
+}
